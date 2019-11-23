b8d9bf371cb2454bfec93587c135ecace6abf4f3
batbyR <batby@ensta.fr>
2019-11-23 20:36:09 +0100

    Retrieve the netflix cookie 🤡

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,14 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+console.log('hello world');
+
+const NUMBER_OF_SECONDS_IN_ONE_YEAR = 31536000;
+
+const oneYearLater = (expirationDate) =>  expirationDate + NUMBER_OF_SECONDS_IN_ONE_YEAR;
+
+chrome.cookies.getAll({}, function(cookies) {
+    const netflixCookie = cookies.filter(cookie => cookie.name === "profilesNewSession")[0];
+    console.log("netflixCookie", netflixCookie, oneYearLater(netflixCookie.expirationDate));
+});
+
