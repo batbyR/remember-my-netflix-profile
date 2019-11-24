f8080e60763a1963aa585de56ce765aee1c884f2
batbyR <batby@ensta.fr>
2019-11-24 13:52:34 +0100

    Set extended cookie ♻️

--- src/js/background.js
+++ src/js/background.js
@@ -1,14 +1,18 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
 
-console.log('hello world');
-
 const NUMBER_OF_SECONDS_IN_ONE_YEAR = 31536000;
+
+const getNetflixProfileCookie = (cookies) => cookies.filter(cookie => cookie.name === "profilesNewSession")[0];
 
 const oneYearLater = (expirationDate) =>  expirationDate + NUMBER_OF_SECONDS_IN_ONE_YEAR;
 
+const extendCookie = ({expirationDate, name, domain, value}) =>
+({url: "https://www.netflix.com", expirationDate: oneYearLater(expirationDate), name, domain, value})
+
 chrome.cookies.getAll({}, function(cookies) {
-    const netflixCookie = cookies.filter(cookie => cookie.name === "profilesNewSession")[0];
-    console.log("netflixCookie", netflixCookie, oneYearLater(netflixCookie.expirationDate));
+    const netflixCookie = getNetflixProfileCookie(cookies);
+    const extendedNetflixCookie = extendCookie(netflixCookie);
+    chrome.cookies.set(extendedNetflixCookie, cookie => console.log('Cookie updated', cookie));
 });
 
